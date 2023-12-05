def62dcdcdbd4d2d8ab9cf62dbea08e11a10ffd8
hssan861 <115433855+hssan861@users.noreply.github.com>
2023-12-05 23:44:05 +0100

    fix bug qrcode

--- config/bundles.php
+++ config/bundles.php
@@ -12,16 +12,9 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
-
-
-    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
-
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
-
-
-    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Endroid\QrCodeBundle\EndroidQrCodeBundle::class => ['all' => true],
+    Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
+    Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     VictorPrdh\RecaptchaBundle\RecaptchaBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
-
 ];
