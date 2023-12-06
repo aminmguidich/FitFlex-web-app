97fa1f1b66db09b2348f4b5108a7e9de7c3eca26
hssan861 <115433855+hssan861@users.noreply.github.com>
2023-12-06 22:22:52 +0100

    updateRanimFrontFinall

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,7 +19,7 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
-        return $this->render('home/index.html.twig', [
+        return $this->render('frontend/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
     }
